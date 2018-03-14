4dcd5237c077d67c25bbe665001554e19e400418
FuChai Mead <mead.fuchai@gmail.com>
2018-03-14 17:27:13 -0600

    contact form

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,19 +1,39 @@
 import React from 'react'
+import { Dimmer, Loader, Container } from 'semantic-ui-react';
 
 class Contact extends React.Component {
+  state={ loaded: false }
+
   componentDidMount() {
   	const script = document.createElement('script');
     script.src = 'https://www.123formbuilder.com/embed/3529579.js';
     script.async = true;
     document.body.appendChild(script);
+    this.setState({ loaded: true })
   }
 
   render(){
-    return(
+    if (this.state.loaded){
+      return(
       <div>
-        <script type="text/javascript" defer src="https://www.123formbuilder.com/embed/3529579.js" data-role="form" data-default-width="650px"></script>
+        <script 
+          type="text/javascript" 
+          defer 
+          src="https://www.123formbuilder.com/embed/3529579.js" 
+          data-role="form" 
+          data-default-width="650px">
+        </script>  
       </div>
     )
+  } else {
+    return(
+      <Container style={{height: '400px'}}>
+        <Dimmer active inverted>
+          <Loader>Loading</Loader>
+        </Dimmer>
+      </Container>
+      )
+    }
   }
 }
 
